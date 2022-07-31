ec3a882d50a63782637d9ab4d378f8b0304b2edd
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-07-31 13:59:03 +0700

    Initial Commit

---
+++ TopDrama/TopDramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopDramaApp.swift
+//  TopDrama
+//
+//  Created by Giang Le on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TopDramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
